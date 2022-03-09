a0f3e7b47be5c391c3af5c1b1e4a3042377f08e3
od-c0d3r <omar_rashad@aol.com>
2022-03-09 11:10:13 +0200

    Design: create button to show new messages

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from "react"
-import PropTypes from "prop-types"
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMessage } from "../state/message/messageSlice";
-
 
 const Greeting = () => {
   const dispatch = useDispatch();
   const { message } = useSelector((state) => state.message);
 
-  useEffect(() => dispatch(fetchMessage()), [])
-
   return (
     <>
+      <div class="d-flex justify-content-center">
+        <button onClick={() => dispatch(fetchMessage())}>Show message</button>
+      </div>
+      <br />
       {message.message_content}
     </>
   );
